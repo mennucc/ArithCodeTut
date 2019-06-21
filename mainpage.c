f90a21bb8ea262414939467e1b819d991b253fac
Andrea C G Mennucci <andrea.mennucci@gmail.com>
2019-06-21 08:56:48 +0200

    doxygen main page

---
+++ mainpage.c
@@ -0,0 +1,37 @@
+
+/*! \mainpage The StiefelCurve library
+
+  \section intro_sec Introduction
+
+  This library implements the code that the arithmetic coding compression.
+
+  You may read 
+  <a href="https://en.wikipedia.org/wiki/Arithmetic_coding">
+  the wikipedia page on the argument to learn what this is about.</a>,
+
+
+  \section compile_sec Compiling
+
+  The code was prepared to be compiled with gcc
+
+  \section install_sec Using as a library
+
+  The code for the arithmetic encoder/decoder is contained in the file arith_code.cc
+
+  It is then included in all other files as needed.
+
+  \section code_sec Overview of the code
+
+  The main are objects are the classes Encoder and  Decoder.
+
+  The classes are documented, see the above menus.
+
+  \section Examples
+
+  The programs arith_file_2.cc and  arith_file.cc  can compress
+
+  \section code_sec Testing the code
+
+  The program  arith_test.cc can test the code in many different ways.
+
+ */
