ff0a3fa500baee911f30163f13660fb057029756
Andrea C G Mennucci <andrea.mennucci@gmail.com>
2019-06-21 09:14:22 +0200

    mainpage

--- mainpage.c
+++ mainpage.c
@@ -3,12 +3,34 @@
 
   \section intro_sec Introduction
 
-  This library implements the code that the arithmetic coding compression.
+  This library implements  arithmetic coding compression.
 
   You may read 
   <a href="https://en.wikipedia.org/wiki/Arithmetic_coding">
   the wikipedia page on the argument to learn what this is about.</a>,
 
+  The library is all contained in the file arith_code.cc
+
+  The main objects in it  are the classes Encoder and  Decoder.
+
+  The Encoder class encodes a sequence of symbols;
+  each symbol is an integer number in the range 0...max_symbol-1 ;
+  the Encoder produces an output sequence of bits.
+
+  The Decoder class decodes the sequence of bits and recovers the sequence of symbols.
+
+  This code is very versatile:
+
+ <ul><li> it can encode sequences of symbols
+  where each symbol has its own random distribution,
+  and even "max_symbol" may change;
+ <li> the output of the encoder/decoder can be processed
+   using "callbacks" or by polling ("callbacks" are recommended, though)
+ <li> the algorithm can work in reverse:
+   the decoder can decode a random i.i.d. string of bits
+   and convert it to a stream of symbols with desired distribution
+  (which can also be re-encoded to the original string of bits)
+ </ul>
 
   \section compile_sec Compiling
 
@@ -22,13 +44,14 @@
 
   \section code_sec Overview of the code
 
-  The main are objects are the classes Encoder and  Decoder.
-
-  The classes are documented, see the above menus.
+  All classes, variables and functions  are documented, see the above menus.
 
   \section Examples
 
-  The programs arith_file_2.cc and  arith_file.cc  can compress
+  The program arith_simple encodes/decodes standard input to standard output.
+  If you wish to learn to use the library, you can start by reading the source code arith_simple.cc
+ 
+  The programs arith_file_2.cc and  arith_file.cc  can compress and decompress files.
 
   \section code_sec Testing the code
 
